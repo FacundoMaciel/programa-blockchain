3b1193a2ecaa4bb0840127829027bf20acd5e7a3
FacundoMaciel <facundo.maciel@geti.cl>
2026-03-24 10:43:34 -0300

    Validation added for modify with setFee function by admin only

--- Programa/Adder.sol
+++ Programa/Adder.sol
@@ -7,16 +7,29 @@ import "./Interfaces/IResult.sol";
 contract Adder {
 
     // Variables
-    address public result;
+    address public resultado;
+    address public admin;
+    uint256 public fee;
 
-    constructor(address result_) {
-        result = result_;
+    constructor(address resultado_, address admin_){
+        resultado = resultado_;
+        admin = admin_;
+        fee = 5;
     }
+
+    // Modifiers
+
+    // Events
 
     // External functions
     function addition(uint256 num1_, uint256 num2_) external {
-        uint256 results_ = num1_ + num2_;
-        IResult(result).setResultado(results_);
+        uint256 resultado_ = num1_ + num2_;
+        IResult(resultado).setResultado(resultado_);
+    }
+
+    function setFee(uint256 newFee_) external {
+        if (msg.sender != admin) revert();
+        fee = newFee_;
     }
 
     // Internal functions
